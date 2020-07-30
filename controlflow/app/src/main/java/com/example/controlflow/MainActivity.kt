2eab57fd9492a652c0de904c14281c96e71c7b5b
lienero <lienero@naver.com>
2020-07-31 01:17:28 +0900

    0731

--- controlflow/app/src/main/java/com/example/controlflow/MainActivity.kt
+++ controlflow/app/src/main/java/com/example/controlflow/MainActivity.kt
@@ -11,9 +11,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var ball = 4
-        if (ball > 3) {
+        if (ball > 3) { //ball이 3보다 크면 여기 블록의 코드가 실행됩니다.
             Log.d("ControlFlow", "4볼로 출루합니다." )
-        } else {
+        } else { // 그렇지 않으면 else 다음에 있는 블록의 코드가 실행됩니다.
             Log.d("ControlFlow", "타석에서 다음 타구를 기다립니다.")
         }
 
